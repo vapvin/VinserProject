ac8526bbc0915d14187507123f265a15ddccbc99
vapvin <tpehdj12@daum.net>
2020-02-25 18:09:00 +0900

    React Apollo Settings

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom';
 import client from './apollo';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);

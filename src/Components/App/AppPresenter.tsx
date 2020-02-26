5a2bcc3775ddb83c20fd48411e37de2e85eb0902
vapvin <tpehdj12@daum.net>
2020-02-26 11:01:26 +0900

    React With Typescript

---
+++ src/Components/App/AppPresenter.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+interface IProps {
+  isLoggedIn: boolean;
+}
+const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) =>
+  isLoggedIn ? <span>Your In</span> : <span>Yr out</span>;
+
+AppPresenter.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
+export default AppPresenter;

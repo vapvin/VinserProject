a21eda41734731609d6dddc3fd7765a358b91f52
vapvin <tpehdj12@daum.net>
2020-05-27 18:17:00 +0900

    Fuck Error < Query Check

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import {RouteComponentProps} from 'react-router-dom';
+import React from "react";
+import { Mutation } from "react-apollo";
+import { RouteComponentProps } from "react-router-dom";
 import { toast } from "react-toastify";
-import PhoneLoginPresenter from './PhoneLoginPresenter';
+import PhoneLoginPresenter from "./PhoneLoginPresenter";
+import { PHONE_SIGN_IN } from "./PhoneQueries";
 
 interface IState {
-    countryCode: string;
-    phoneNumber: string;
+  countryCode: string;
+  phoneNumber: string;
 }
 
+interface IMutationInterface {
+  phoneNumber: string;
+}
 
-class PhoneLoginContainer extends React.Component<RouteComponentProps<any>, IState> {
+class PhoneSignInMutation extends Mutation<any, IMutationInterface> {}
 
-    public state = {
-        countryCode:"+82",
-        phoneNumber: ""
-    }
-    public render(){
-        const {countryCode, phoneNumber} = this.state;
-        return <PhoneLoginPresenter countryCode={countryCode} phoneNumber={phoneNumber} />
-    }
+class PhoneLoginContainer extends React.Component<
+  RouteComponentProps<any>,
+  IState
+> {
+  public state = {
+    countryCode: "+82",
+    phoneNumber: ""
+  };
+
+  public render() {
+    const { countryCode, phoneNumber } = this.state;
+    return (
+      <PhoneSignInMutation
+        mutation={PHONE_SIGN_IN}
+        variables={{
+          phoneNumber: `${countryCode}${phoneNumber}`
+        }}
+      >
+        {(mutation, { loading }) => {
+          const onSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+            event.preventDefault();
+            const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(
+              `${countryCode}${phoneNumber}`
+            );
+            if (isValid) {
+              mutation();
+            } else {
+              toast.error("Please write a valid phone number");
+            }
+          };
+          return (
+            <PhoneLoginPresenter
+              countryCode={countryCode}
+              phoneNumber={phoneNumber}
+              onInputChange={this.onInputChange}
+              onSubmit={onSubmit}
+              loading={loading}
+            />
+          );
+        }}
+      </PhoneSignInMutation>
+    );
+  }
+
+  public onInputChange: React.ChangeEventHandler<
+    HTMLInputElement | HTMLSelectElement
+  > = event => {
+    const {
+      target: { name, value }
+    } = event;
+    this.setState({
+      [name]: value
+    } as any);
+  };
 }
 
 export default PhoneLoginContainer;
--- src/Routes/PhoneLogin/PhoneLoginPresenter.tsx
+++ src/Routes/PhoneLogin/PhoneLoginPresenter.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import BackArrow from "../../Components/BackArrow";
 import Helmet from "react-helmet";
-import PropTypes from "prop-types";
+import BackArrow from "../../Components/BackArrow";
 import Input from "../../Components/Input";
 import countries from "../../countries";
 import styled from "../../typed-components";
@@ -24,7 +23,7 @@ const Title = styled.h2`
 
 const CountrySelect = styled.select`
   font-size: 20px;
-  color: #2c3e50;
+  color: "#2c3e50";
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
@@ -54,49 +53,71 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
-
 interface IProps {
-    countryCode: string;
-    phoneNumber: string;
+  countryCode: string;
+  phoneNumber: string;
+  onInputChange: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
+  onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  loading: boolean;
 }
 
 const PhoneLoginPresenter: React.SFC<IProps> = ({
-        countryCode,
-        phoneNumber
-    }) => (
-    <Container>
-        <Helmet>
-            <title>Phone Login | Vinser</title>
-        </Helmet>
-        <BackArrowExtended backTo={"/"} />
-        <Title>Enter your mobile number</Title>
-        <CountrySelect value={countryCode}>
-            {countries.map((country, index) => (
-                <CountryOption key={index} value={country.dial_code}>
-                    {country.flag} {country.name} ({country.dial_code})
-                </CountryOption>
-            ))}
-        </CountrySelect>
-        <Form>
-            <Input placeholder={"010 1234 5678"} value={phoneNumber} />
-            <Button>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill={"white"}
-                >
-                    <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
-                </svg>
-            </Button>
-        </Form>
-    </Container>
+  countryCode,
+  phoneNumber,
+  onInputChange,
+  onSubmit,
+  loading
+}) => (
+  <Container>
+    <Helmet>
+      <title>Phone Login | Vinser</title>
+    </Helmet>
+    <BackArrowExtended backTo={"/"} />
+    <Title>Enter your mobile number</Title>
+    <CountrySelect
+      value={countryCode}
+      name={"countryCode"}
+      onChange={onInputChange}
+    >
+      {countries.map((country, index) => (
+        <CountryOption key={index} value={country.dial_code}>
+          {country.flag} {country.name} ({country.dial_code})
+        </CountryOption>
+      ))}
+    </CountrySelect>
+    <Form onSubmit={onSubmit}>
+      <Input
+        placeholder={"053 690 2129"}
+        value={phoneNumber}
+        name={"phoneNumber"}
+        onChange={onInputChange}
+      />
+      <Button>
+        {loading ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M13.75 22c0 .966-.783 1.75-1.75 1.75s-1.75-.784-1.75-1.75.783-1.75 1.75-1.75 1.75.784 1.75 1.75zm-1.75-22c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm10 10.75c.689 0 1.249.561 1.249 1.25 0 .69-.56 1.25-1.249 1.25-.69 0-1.249-.559-1.249-1.25 0-.689.559-1.25 1.249-1.25zm-22 1.25c0 1.105.896 2 2 2s2-.895 2-2c0-1.104-.896-2-2-2s-2 .896-2 2zm19-8c.551 0 1 .449 1 1 0 .553-.449 1.002-1 1-.551 0-1-.447-1-.998 0-.553.449-1.002 1-1.002zm0 13.5c.828 0 1.5.672 1.5 1.5s-.672 1.501-1.502 1.5c-.826 0-1.498-.671-1.498-1.499 0-.829.672-1.501 1.5-1.501zm-14-14.5c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2zm0 14c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill={"white"}
+          >
+            <path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" />
+          </svg>
+        )}
+      </Button>
+    </Form>
+  </Container>
 );
-
-PhoneLoginPresenter.propTypes = {
-    countryCode: PropTypes.string.isRequired,
-    phoneNumber: PropTypes.string.isRequired
-};
 
 export default PhoneLoginPresenter;
